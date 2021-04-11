0635695e07af29f9bad29d60264ad226eaab21a5
dikers <dikers@gmail.com>
2021-04-11 16:58:45 +0800

    添加 哈密尔顿回路算法

---
+++ python/graph/HamiltonLoop.py
@@ -0,0 +1,67 @@
+from graph.Graph import Graph
+
+
+class HamiltonLoop:
+
+    def __init__(self, G: Graph):
+        self.G = G
+        self.visited = [False] * G.get_v()
+        self._pre = [-1] * G.get_v()
+        self._end = -1
+        self.dfs(0, 0)
+
+
+
+    def dfs(self, v: int, parent: int)-> bool:
+        if self.visited[v]:
+            return
+        self.visited[v] = True
+        self._pre[v] = parent
+        # print(v)
+
+        for w in self.G.get_adj(v):
+            if not self.visited[w]:
+                if self.dfs(w, v):
+                    return True
+            elif w == 0 and self.all_visited():
+
+                self._end = v    # 最后一个节点的parent
+                return True   
+        self.visited[v] = False         # 回溯
+        return False
+
+    def result(self):
+        res = []
+        if self._end == -1:
+            return res
+
+
+        cur = self._end
+
+        while cur != 0:
+            res.append(cur)
+            cur = self._pre[cur]
+        res.append(0)
+        # print("res ", res)
+        res.reverse()
+        # print("res ", res)
+        return res
+                
+    def all_visited(self) -> bool:
+
+        for v in self.visited:
+            if not v:
+                return False
+        return True
+
+
+
+if __name__ == '__main__':
+
+    graph = Graph('./data/hamilton2.txt')
+    print(graph)
+    hamilton = HamiltonLoop(graph)
+    print("end: ", hamilton._end)
+    print(hamilton.result())
+
+    
